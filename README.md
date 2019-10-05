3a9b324e1f3c8df9bc53a8455ab18d626d1b811a
Pramana <pramanadiputra@gmail.com>
2019-10-05 10:44:33 +0700

    Adding tagging functionality
    Adding tagging functionality such as AddTags and Increment counting tags when tags are assigning

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ The goal of this package is to enable Laravel web app to support tagging system.
 Let me know if your Laravel web apps also use this package to support tag system. I use it too for Pedia Indonesia web app.
 
 - Pedia Indonesia (https://pedia.id)
+
--- src/Tagging.php
+++ src/Tagging.php
@@ -3,11 +3,86 @@
 namespace Codetensor\Tagger;
 
 use Codetensor\Tagger\Models\Tag;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 
 Trait Tagging
 {
+	/**
+	 * Return collection of tagged rows related to the tagged model
+	 *
+	 * @return \Illuminate\Database\Eloquent\Collection
+	 */
 	public function tags()
 	{
 		return $this->morphToMany(Tag::class, 'taggable');
+	}
+
+
+	/**
+	 *
+	 */
+	public function tag($tags)
+	{
+		$this->addTags($this->getWorkableTags($tags));
+	}
+
+	/**
+	 *
+	 */
+	private function addTags(Collection $tags)
+	{
+		$sync = $this->tags()->syncWithoutDetaching($tags->pluck('id')->toArray());
+
+		foreach (Arr::get($sync, 'attached') as $attachId) {
+			$tags->where('id', $attachId)->first()->increment('count');
+		}
+	}
+
+	/**
+	 *
+	 */
+	private function getWorkableTags($tags)
+	{
+		if (is_array($tags)) {
+			return $this->getTagModels($tags);
+		}
+
+		if ($tags instanceof Model) {
+			return $this->getTagModels([$tags->slug]);
+		}
+
+		return $this->filterTagsCollection($tags);
+	}
+
+	private function filterTagsCollection(Collection $tags)
+	{
+		return $tags->filter(function ($tag) {
+			return $tag instanceof Model;
+		});
+	}
+
+	/**
+	 *
+	 *
+	 * @return \Illuminate\Database\Eloquent\Collection
+	 */
+	private function getTagModels(array $tags)
+	{
+		return Tag::whereIn('slug', $this->normaliseTagNames($tags))->get();
+	}
+
+	/**
+	 * Normalise the tag names to be slugs
+	 *
+	 * @return array
+	 */
+	private function normaliseTagNames(array $tags)
+	{
+		return array_map(function ($tag) {
+			return Str::slug($tag);
+		}, $tags);
 	}
 }
